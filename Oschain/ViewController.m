48c07753ef565429c9790dc40a491f0badcd8dec
wss <wang_shaoshuai@sina.cn>
2018-07-10 16:44:27 +0800

    update

--- Oschain/ViewController.m
+++ Oschain/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     [self checkAppVersion];
     
+    //123
     self.view.backgroundColor = [UIColor colorWithHexString:@"#DD99AA"];
     
 }
